ccbcf6489640db0e8247fe7c13771ef30cd8be79
nberr <nberriochoa@gmail.com>
2023-07-20 11:24:25 -0600

    basic header and footer

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,34 @@
+import { Navbar, Nav, Container, NavbarBrand } from 'react-bootstrap';
+import { FaShoppingCart, FaUser } from 'react-icons/fa';
+
+const Header = () => {
+  return (
+    <header>
+        {/* navbar settings 
+            bg - background color
+            variant - 
+            expand - when should the menu turn into a hamburger? lg makes it turn fairly quickly so we can use md
+            collapseOnSelect - when a item is selected the menu will collapse
+        */}
+        <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+            {/* container so inner elements don't stretch to the edge of the browser */}
+            <Container>
+                {/* brand name */}
+                <NavbarBrand href="/">ProShop</NavbarBrand>
+                {/*  */}
+                <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                {/* components that will be hidden in the hamburger menu */}
+                <Navbar.Collapse id="basic-navbar-nav">
+                    {/* ms-auto aligns links to right */}
+                    <Nav className="ms-auto">
+                        <Nav.Link href="/cart"><FaShoppingCart/>Cart</Nav.Link>
+                        <Nav.Link href="/login"><FaUser/>Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
