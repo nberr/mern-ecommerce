748e3a0f0b6e32eefc4166519fb4bb16e2f844de
nberr <nberriochoa@gmail.com>
2023-07-26 09:31:20 -0600

    user auth backend

--- backend/server.js
+++ backend/server.js
@@ -14,6 +14,10 @@ connectDB();
 
 const app = express();
 
+// body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
 app.get('/', (req, res) => {
     res.send('API is running...')
 });
