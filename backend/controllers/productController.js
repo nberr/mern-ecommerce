ea1b28186576a1b52d6a2376b37576cedc1d975e
nberr <nberriochoa@gmail.com>
2023-07-31 11:54:00 -0600

    create a blank product

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -23,4 +23,24 @@ const getProductById = asyncHandler( async (req, res) => {
     throw new Error('Resource not found');
 });
 
-export { getProducts, getProductById };
+// @desc Create a product
+// @route POST /api/products
+// @access Private/Admin
+const createProduct = asyncHandler( async (req, res) => {
+    const product = new Product({
+        name: 'Sample name',
+        price: 0,
+        user: req.user._id,
+        image: 'images/sample.jpg',
+        brand: 'Sample brand',
+        category: 'Sample Category',
+        countInStock: 0,
+        numReviews: 0,
+        description: 'Sample description',
+    });
+
+    const createProduct = await product.save();
+    res.status(201).json(createProduct);
+});
+
+export { getProducts, getProductById, createProduct };
